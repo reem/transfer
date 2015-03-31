ff81bfb8d1080e4505fcb9f696c4069afd9956c8
Jonathan Reem <jonathan.reem@gmail.com>
2015-03-30 23:24:44 -0700

    (feat) Implement readable and writable for Acceptor.

--- src/rt/acceptor.rs
+++ src/rt/acceptor.rs
@@ -4,7 +4,8 @@ use mio::{NonBlock, EventLoop, ReadHint, Token};
 use mio::tcp::TcpListener;
 use syncbox::util::Run;
 
-use rt::loophandler::LoopHandler;
+use rt::loophandler::{LoopHandler, Registration};
+use rt::connection::Connection;
 
 use Handler as HttpHandler;
 
@@ -22,16 +23,35 @@ impl Acceptor {
         }
     }
 
-    pub fn readable<R: Run>(handler: &mut LoopHandler<R>,
+    pub fn readable<R: Run>(mut handler: &mut LoopHandler<R>,
                             event_loop: &mut EventLoop<LoopHandler<R>>,
                             token: Token, hint: ReadHint) {
+        let (httphandler, connection) = {
+            if let &mut Registration::Acceptor(ref mut acceptor) = &mut handler.slab[token] {
+                (acceptor.handler.clone(), acceptor.listener.accept())
+            } else {
+                unsafe { debug_unreachable!("LoopHandler yielded connection to acceptor.") }
+            }
+        };
 
+        match connection {
+            Ok(Some(connection)) => {
+                let conn = Connection::new(connection, httphandler);
+                handler.register(Registration::Connection(conn));
+            },
+
+            Ok(None) => {
+                unsafe { debug_unreachable!("Incorrect readable hint.") }
+            }
+
+            Err(_) => { }
+        };
     }
 
     pub fn writable<R: Run>(handler: &mut LoopHandler<R>,
                             event_loop: &mut EventLoop<LoopHandler<R>>,
                             token: Token) {
-
+        unsafe { debug_unreachable!("Received writable hint on an acceptor.") }
     }
 }
 
