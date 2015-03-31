8b6f67c98689281a29c0e9ed6da590c16c9cf7cf
Jonathan Reem <jonathan.reem@gmail.com>
2015-03-31 13:51:00 -0700

    (feat) Add Handlers and Allocators to acceptor.

--- src/rt/acceptor.rs
+++ src/rt/acceptor.rs
@@ -2,33 +2,39 @@ use std::sync::Arc;
 
 use mio::{NonBlock, EventLoop, ReadHint, Token};
 use mio::tcp::TcpListener;
-use syncbox::util::Run;
+use iobuf::Allocator;
 
 use rt::loophandler::{LoopHandler, Registration};
 use rt::connection::Connection;
 
+use prelude::*;
 use Handler as HttpHandler;
 
 pub struct Acceptor {
     listener: NonBlock<TcpListener>,
-    handler: Arc<Box<HttpHandler>>
+    handler: Arc<Box<HttpHandler>>,
+    allocator: Arc<Box<Allocator>>
 }
 
 impl Acceptor {
     pub fn new(listener: NonBlock<TcpListener>,
-               handler: Box<HttpHandler>) -> Acceptor {
+               handler: Box<HttpHandler>,
+               allocator: Arc<Box<Allocator>>) -> Acceptor {
         Acceptor {
             listener: listener,
-            handler: Arc::new(handler)
+            handler: Arc::new(handler),
+            allocator: allocator
         }
     }
 
     pub fn readable<R: Run>(mut handler: &mut LoopHandler<R>,
                             event_loop: &mut EventLoop<LoopHandler<R>>,
                             token: Token, hint: ReadHint) {
-        let (httphandler, connection) = {
+        let (httphandler, connection, allocator) = {
             if let &mut Registration::Acceptor(ref mut acceptor) = &mut handler.slab[token] {
-                (acceptor.handler.clone(), acceptor.listener.accept())
+                (acceptor.handler.clone(),
+                 acceptor.listener.accept(),
+                 acceptor.allocator.clone())
             } else {
                 unsafe { debug_unreachable!("LoopHandler yielded connection to acceptor.") }
             }
@@ -36,7 +42,7 @@ impl Acceptor {
 
         match connection {
             Ok(Some(connection)) => {
-                let conn = Connection::new(connection, httphandler);
+                let conn = Connection::new(connection, httphandler, allocator);
                 handler.register(Registration::Connection(conn));
             },
 
