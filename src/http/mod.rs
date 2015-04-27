9e7f7532199142042cc635df0a7d77effe18672d
Jonathan Reem <jonathan.reem@gmail.com>
2015-04-26 23:38:33 -0400

    (feat) Add scaffold Request and Response types to http

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -5,29 +5,27 @@ use eventual::Sender;
 use rt::connection::Snapshot;
 use rt::Metadata;
 
-// FIXME: This should go away
-use rt::connection::Response;
-
 use prelude::*;
 
 pub mod parser;
 
-struct Request;
-
-pub fn handle_connection(
-    metadata: Metadata, handler: Arc<Box<Handler>>,
-    snapshots: Stream<Snapshot, Error>, responses: Sender<Response, Error>) {
-
-    let (requests_tx, requests_rx) = Stream::<Request, Error>::pair();
-
-    snapshots.each(move |snapshot| {
-        match snapshot {
-            Snapshot::Head(head) => {
-
-            },
-            Snapshot::Body(_) => {}
-        }
-    }).fire();
+pub struct Request {
+    raw: parser::RawRequest
 }
 
+pub struct Response {
+    raw: parser::RawResponse
+}
+
+impl From<parser::RawRequest> for Request {
+    fn from(raw: parser::RawRequest) -> Request {
+        Request { raw: raw }
+    }
+}
+
+impl From<parser::RawResponse> for Response {
+    fn from(raw: parser::RawResponse) -> Response {
+        Response { raw: raw }
+    }
+}
 
