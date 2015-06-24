4edfc81a30bb193cd36a6686e8d2c8497ba321dc
Jonathan Reem <jonathan.reem@gmail.com>
2015-06-24 00:00:50 -0700

    (feat) Implement http2 Frame and Payload parsing/conversion.

--- src/http/parser.rs
+++ src/http/parser.rs
@@ -1,13 +1,14 @@
 use iobuf::{AROIobuf, RWIobuf};
 
-use std::{raw, mem};
+use std::{raw, slice, mem};
 
 use util::TypedAROIobuf;
 use prelude::*;
 
 pub use http2parse::{
     FrameHeader, Priority, SizeIncrement,
-    ErrorCode, Setting, StreamIdentifier
+    ErrorCode, Setting, StreamIdentifier,
+    ParserSettings
 };
 
 pub struct Frame {
@@ -15,9 +16,28 @@ pub struct Frame {
     pub payload: Payload
 }
 
+impl Frame {
+    pub fn parse(header: FrameHeader, buf: AROIobuf,
+                 settings: ParserSettings) -> Result<Frame> {
+        let bytes = unsafe { buf.as_window_slice() };
+        let raw = try!(::http2parse::Frame::parse(header, bytes, settings));
+
+        Ok(Frame {
+            header: raw.header,
+            payload: Payload::convert(raw.payload, &buf)
+        })
+    }
+}
+
 pub enum Error {
     Parse(::http2parse::Error),
     Incomplete
+}
+
+impl From<::http2parse::Error> for Error {
+    fn from(err: ::http2parse::Error) -> Error {
+        Error::Parse(err)
+    }
 }
 
 pub type Result<T> = ::std::result::Result<T, Error>;
@@ -43,6 +63,50 @@ pub enum Payload {
     },
     WindowUpdate(SizeIncrement),
     Continuation(AROIobuf)
+}
+
+impl Payload {
+    fn convert(raw: ::http2parse::Payload, buf: &AROIobuf) -> Payload {
+        use http2parse::Payload as Raw;
+
+        match raw {
+            Raw::Data { data } =>
+                Payload::Data(unsafe { convert_slice(buf, data) }),
+            Raw::Headers { priority, block } =>
+                Payload::Headers {
+                    priority: priority,
+                    block: unsafe { convert_slice(buf, block) }
+                },
+            Raw::Priority(priority) => Payload::Priority(priority),
+            Raw::Reset(error) => Payload::Reset(error),
+            Raw::Settings(settings) => {
+                let settings_buf = unsafe {
+                    slice::from_raw_parts(
+                        settings.as_ptr() as *const u8,
+                        settings.len() * mem::size_of::<Setting>())
+                };
+
+                Payload::Settings(unsafe {
+                    TypedAROIobuf::new(convert_slice(buf, settings_buf))
+                })
+            },
+            Raw::PushPromise { promised, block } =>
+                Payload::PushPromise {
+                     promised: promised,
+                     block: unsafe { convert_slice(buf, block) }
+                },
+            Raw::Ping(opaque) => Payload::Ping(opaque),
+            Raw::GoAway { last, error, data } =>
+                Payload::GoAway {
+                    last: last,
+                    error: error,
+                    data: unsafe { convert_slice(buf, data) }
+                },
+            Raw::WindowUpdate(sz) => Payload::WindowUpdate(sz),
+            Raw::Continuation(data) =>
+                Payload::Continuation(unsafe { convert_slice(buf, data) })
+        }
+    }
 }
 
 /// Convert a slice from a given AROIobuf into an AROIobuf over the same region.
