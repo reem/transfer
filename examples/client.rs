b6cf526e5eaa3d6014692587fe7d16fb7ea580db
Jonathan Reem <jonathan.reem@gmail.com>
2015-10-25 18:00:45 -0500

    Fix the client example to compile with and without the random feature.

--- examples/client.rs
+++ examples/client.rs
@@ -1,31 +1,34 @@
 extern crate http2parse;
 extern crate rand;
 
-use std::net::TcpStream;
-use std::io::Write;
 use std::thread;
 
-use http2parse::Frame;
-
 fn main() {
-    let mut guards = vec![];
-
-    for _ in 0..10 {
-        let guard = thread::spawn(move || {
-            let mut stream = TcpStream::connect("localhost:3000").unwrap();
-
-            let frame = ::rand::random::<Frame<'static>>();
-            println!("Sending Frame: {:?}", frame);
-
-            let mut buf = vec![0; 2000];
-            let frame_len = frame.encode(&mut buf);
-
-            stream.write(&buf[..frame_len]).unwrap();
-        });
-
-        guards.push(guard);
-    }
-
+    let guards = (0..10).map(|_| { go() }).collect::<Vec<_>>();
     for guard in guards { guard.join().unwrap(); }
 }
 
+#[cfg(not(feature = "random"))]
+fn go() -> thread::JoinHandle<()> {
+    panic!("Running client without features = random.");
+}
+
+#[cfg(feature = "random")]
+fn go() -> thread::JoinHandle<()> {
+    use std::net::TcpStream;
+    use std::io::Write;
+
+    use http2parse::Frame;
+
+    thread::spawn(move || {
+        let mut stream = TcpStream::connect("localhost:3000").unwrap();
+
+        let frame = ::rand::random::<Frame<'static>>();
+        println!("Sending Frame: {:?}", frame);
+
+        let mut buf = vec![0; 2000];
+        let frame_len = frame.encode(&mut buf);
+
+        stream.write(&buf[..frame_len]).unwrap();
+    })
+}
